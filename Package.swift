b3a0acfe823f9cf6dfe6d2343b0421f094444123
Joseph Mattello <mail@joemattiello.com>
2023-02-10 05:49:26 -0500

    Fix issue with executable target, commented out
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- Package.swift
+++ Package.swift
@@ -25,11 +25,15 @@ let package = Package(
             type: .static,
             targets: ["Harmony"]
         ),
-        .executable(name: "Example", targets: ["Example"])
+        // Removed example due to circular dependency with sub-modules, HarmonyDrive/HarmonyDropbox
+//        .executable(name: "Example", targets: ["Example"])
     ],
     dependencies: [
          .package(url: "https://github.com/JoeMatt/Roxas.git", from: "1.0.2"),
-//        .package(path: "../Roxas")
+         //        .package(path: "../Roxas")
+//         .package(url: "https://github.com/JoeMatt/Harmony-Drive.git", from: "1.0.0"),
+//         .package(url: "https://github.com/JoeMatt/Harmony-Dropbox.git", from: "1.0.0"),
+//
     ],
     targets: [
         .target(
@@ -46,10 +50,18 @@ let package = Package(
                 .linkedFramework("CoreData")
             ]
         ),
-        .target(
-            name: "Example",
-            dependencies: ["Roxas"]
-        ),
+//        .executableTarget(
+//            name: "Example",
+//            dependencies: ["Harmony", "Roxas", "HarmonyDrive", "HarmonyDropbox"],
+//            resources: [
+//                .copy("Resources/GoogleService-Info.plist"),
+//                .process("Resources/UIKit")
+//            ],
+//            linkerSettings: [
+//                .linkedFramework("UIKit"),
+//                .linkedFramework("CoreData")
+//            ]
+//        ),
         .testTarget(
             name: "HarmonyTests",
             dependencies: ["Harmony"]
